3ca786ae305eda5489112b0b25d710d4a75f0c1a
Takeshi Sagara <sagara@info-proto.com>
2023-08-01 03:50:21 +0000

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # ST_InternalCenter
 PostGIS Function which calculate internal-center point of a polygon.
+
+The SRID of the polygon must be 4326.
+
+## How to use
+
+To install this function, run the following command on a PostgreSQL database with PostGIS installed.
+
+$ psql -f st_internalcenter.sql
+
+The installed functions are used in SQL as follows.
+
+```
+SELECT ST_AsText(ST_InternalCenter(g))
+FROM  ST_GeomFromText('POLYGON((0 2, -1 1,0 0, 1 0, -0.5 1, 1 2, 0 2))', 4326)  AS g ;
+                  st_astext
+---------------------------------------------
+ POINT(-0.312820512820513 1.385897435897436)
+(1 row)
+```
+
+## How to calculate the center point
+
+- Basically, the centroid is used. (using ST_Centroid)
+
+- If the centroid is not inside the polygon,
+  cut the polygon with a straight line connecting
+  the centroid and the nearest point on the polygon
+  and use the center point of the intersection.
+
+- When the line does not intersect the polygon
+  (due to calculation errors), a buffer with a radius of
+  1-meter is generated around the point on the polygon
+  closest to the centroid, and use the centroid of
+  the intersection of the buffer and the polygon.
+
+## Author
+
+- Takeshi Sagara <sagara@info-proto.com>
+
+## License
+
+The MIT License.
---
+++ st_internalcenter.sql
@@ -0,0 +1,52 @@
+DROP FUNCTION IF EXISTS ST_Internalcenter(geometry);
+
+CREATE FUNCTION ST_Internalcenter(gm_in geometry)
+RETURNS geometry 
+AS $$
+DECLARE 
+    geom_type text;
+    gm_polygon geometry;
+    gm_center  geometry;
+    gm_closest geometry;
+    gm_line geometry;
+    gm_intersection geometry;
+    dx double precision;
+    dy double precision;
+BEGIN
+    SELECT GeometryType(gm_in) INTO geom_type;
+    IF geom_type = 'MULTIPOLYGON' THEN
+        gm_polygon := ST_GeometryN(gm_in, 1);
+    ELSIF geom_type = 'POLYGON' THEN
+        gm_polygon := gm_in;
+    ELSE
+        RAISE EXCEPTION 'Param must be Polygon or MultiPolygon';
+    END IF;
+    gm_center := ST_Centroid(gm_polygon);
+
+    IF NOT ST_Intersects(gm_center, gm_polygon) THEN
+        SELECT ST_ClosestPoint(gm_polygon, gm_center) INTO gm_closest;
+        -- RAISE NOTICE 'ClosestPoing: %', ST_AsGeoJSON(gm_closest);
+        dx := ST_X(gm_closest) - ST_X(gm_center);
+        dy := ST_Y(gm_closest) - ST_Y(gm_center);
+        SELECT ST_MakeLine(
+            gm_center,
+            ST_SetSRID(ST_Point(
+                ST_X(gm_center) + 10.0 * dx, ST_Y(gm_center) + 10.0 * dy
+                ), 4326)
+            ) INTO gm_line;
+        -- RAISE NOTICE 'line:%', ST_AsGeoJSON(gm_line);
+        SELECT ST_Intersection(gm_polygon, gm_line) INTO gm_intersection;
+        -- RAISE NOTICE 'intersection(line): %', ST_AsGeoJSON(gm_intersection);
+        IF ST_NPoints(gm_intersection) = 0
+            OR ST_Length(ST_GeometryN(gm_intersection, 1)::geography) < 1.0e-3 THEN
+            gm_intersection := ST_Intersection(
+                ST_Buffer(gm_closest::geography, 1.0)::geometry,
+                gm_polygon
+            );
+            -- RAISE NOTICE 'intersection(polygon): %', ST_AsGeoJSON(gm_intersection);
+        END IF;
+        gm_center := ST_Centroid(ST_GeometryN(gm_intersection, 1));
+    END IF;
+    RETURN gm_center;
+END; 
+$$ LANGUAGE plpgsql VOLATILE;
